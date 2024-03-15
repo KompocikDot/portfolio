db3e8e17429bc4e3bd5453f757bf39ce31f06b58
Kacper Soczko <kacper.soczko@gmail.com>
2024-03-15 13:37:45 +0100

    First prototype of mobile page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'gunmetal': '#333745',
+        'tea': '#C7EFCF',
+        'vista-blue': '#8EA4D2',
+        'deep-pink': '#EC368D',
+        'cerise': '#e63462'
+      }
+    },
+  },
+  plugins: [],
+}
+
