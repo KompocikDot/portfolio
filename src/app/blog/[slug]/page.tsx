2d2834d5d5e14ebc2c8d1e8fd05d1cf0677fddc7
Kacper Soczko <kacper.soczko@gmail.com>
2025-07-06 19:28:08 +0200

    Fix blog post typos

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -10,7 +10,7 @@ export default async function Page({
   return (
     <>
       <Header activePage="blog" />
-      <article className="prose prose-neutral prose-blockquote:text-aquamarine-500 prose-headings:text-white prose-a:text-aquamarine-500 prose-strong:text-white  prose-headings:text-center text-white  mx-auto m-5">
+      <article className="prose prose-neutral prose-blockquote:text-aquamarine-500 prose-headings:text-white prose-a:text-aquamarine-500 prose-strong:text-white  prose-headings:text-center prose-code:text-white prose-figure:text-white text-white  mx-auto m-5">
         <BlogPost />
       </article>
     </>
