0816d7e83d19a3302486f0da0b426b1d2894cbb4
Kacper Soczko <kacper.soczko@gmail.com>
2025-08-24 18:17:52 +0200

    Update padding on mobile devices and set code blocks to overflow-x-auto

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,28 +1,24 @@
-import Header from "@/app/components/header"
+import Header from "@/app/components/header";
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ slug: string }>
+  params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params
-  const { default: BlogPost } = await import(`../content/${slug}.mdx`)
+  const { slug } = await params;
+  const { default: BlogPost } = await import(`../content/${slug}.mdx`);
   return (
     <>
       <Header activePage="blog" />
-      <article className="prose prose-neutral prose-blockquote:text-aquamarine-500 prose-headings:text-white prose-a:text-aquamarine-500 prose-strong:text-white  prose-headings:text-center prose-code:text-white prose-figure:text-white text-white mt-10 sm:w-full mx-auto">
+      <article className="prose max-w-none prose-neutral prose-blockquote:text-aquamarine-500 prose-headings:text-white prose-a:text-aquamarine-500 prose-strong:text-white  prose-headings:text-center prose-code:text-white prose-code:overflow-x-auto prose-figure:text-white text-white mt-10 w-full px-5">
         <BlogPost />
       </article>
     </>
-  )
-
+  );
 }
 
 export function generateStaticParams() {
-  return [
-    { slug: 'firefox-rant' },
-    { slug: 'costly-datediff' },
-  ];
+  return [{ slug: "firefox-rant" }, { slug: "costly-datediff" }];
 }
 
-export const dynamicParams = false
+export const dynamicParams = false;
